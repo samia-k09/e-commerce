3ca42a016dd5d6e46a7b5a5fa12d3e776da0204c
samia-k09 <samiakhan523012@gmail.com>
2026-05-07 16:01:15 +0530

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,1 +1,26 @@
+public class Product {
 
+  
+    int id;
+    String name;
+    double price;
+    int stock;
+
+   
+    Product(int id, String name, double price, int stock) {
+
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    
+    void displayProduct() {
+
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Price: ₹" + price);
+        System.out.println("Stock: " + stock);
+    }
+}
